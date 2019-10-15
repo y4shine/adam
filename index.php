69879c58b34699472c98b3dc026ab25317c4b1cf
y4shine <56487141+y4shine@users.noreply.github.com>
2019-10-15 23:43:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adam's Photography</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+<body>
+    <header id="header">
+        <nav>
+            <ul>
+                <li class="logo"><a href="">Adam's Photography</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="intro">
+        <h1>Adam's Photography</h1>
+        <p>Welcome to our site, we provide awesome services. Hope you stick with us.</p>
+    </section>
+    <aside id="aside">
+        <h3>Categories</h3>
+        <ul>
+            <li><a href="">People</a></li>
+            <li><a href="">Nature</a></li>
+            <li><a href="">House</a></li>
+            <li><a href="">Business</a></li>
+        </ul>
+    </aside>
+    <section id="gallery">
+        <h2>Gallery</h2>
+        <div class="gallery-container">
+            <div class="img-container">
+                <img src="img/business4.jpg" alt="">
+            </div>
+            <div class="img-container">
+                <img src="img/home3.jpg" alt="">
+            </div>
+            <div class="img-container">
+                <img src="img/nature3.jpg" alt="">
+            </div>
+            <div class="img-container">
+                <img src="img/people3.jpg" alt="">
+            </div>
+        </div>
+
+    </section>
+    <section id="services">
+        <h2>Services</h2>
+        <div class="services-container">
+            <div class="service">
+                <div class="service-img">
+                    <img src="img/ad.jpg" alt="">
+                </div>
+                <div class="service-desc">
+                    <h5>People</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ab laudantium perferendis natus nesciunt ad, reiciendis illum ipsum libero laborum recusandae odit ut unde repellendus fugiat adipisci repellat, pariatur deleniti.</p>
+                    <a href="" class="call-to-action">View More</a>
+                </div>
+            </div>
+            <div class="service">
+                <div class="service-img">
+                    <img src="img/business3.jpg" alt="">
+                </div>
+                <div class="service-desc">
+                    <h5>Business</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ab laudantium perferendis natus nesciunt ad, reiciendis illum ipsum libero laborum recusandae odit ut unde repellendus fugiat adipisci repellat, pariatur deleniti.</p>
+                    <a href="" class="call-to-action">View More</a>
+                </div>
+            </div>
+            <div class="service">
+                <div class="service-img">
+                    <img src="img/home2.jpg" alt="">
+                </div>
+                <div class="service-desc">
+                    <h5>Home</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ab laudantium perferendis natus nesciunt ad, reiciendis illum ipsum libero laborum recusandae odit ut unde repellendus fugiat adipisci repellat, pariatur deleniti.</p>
+                    <a href="" class="call-to-action">View More</a>
+                </div>
+            </div>
+            <div class="service">
+                <div class="service-img">
+                    <img src="img/aa.jpg" alt="">
+                </div>
+                <div class="service-desc">
+                    <h5>Nature</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ab laudantium perferendis natus nesciunt ad, reiciendis illum ipsum libero laborum recusandae odit ut unde repellendus fugiat adipisci repellat, pariatur deleniti.</p>
+                    <a href="" class="call-to-action">View More</a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section id="about">
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda tempore officiis accusamus, facilis ducimus repudiandae eius quis harum cupiditate hic quas doloribus. Id natus ut est laudantium, dolores quos laboriosam.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt sunt non temporibus minima adipisci omnis. Blanditiis aperiam omnis eum atque quaerat tenetur repellendus qui similique modi in, maxime odit dolorum!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis eaque cupiditate molestiae similique totam, neque inventore maxime et dolorum tempore temporibus magni praesentium quod molestias iure aut nihil, minus, porro!</p>
+    </section>
+    <section id="contact">
+        <h2>Contact</h2>
+        <form action="">
+            <label for="name">Name:</label>
+            <input type="text" id="name">
+            <label for="email">Email:</label>
+            <input type="email" id="email">
+            <label for="message">Message:</label>
+            <textarea name="" id="message">
+            </textarea>
+        </form>
+    </section>
+    <footer id="footer">
+        <p>© 2019 Copyright:  <a href="#">adamsphotography.com</a></p>
+    </footer>
+
+</body>
+</html>
